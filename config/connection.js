232ef007ea947caf1c4d07c89570282eb1df2974
ijeli <legaspi.jel@gmail.com>
2018-09-18 17:35:22 -0400

    yoyo

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,36 @@
-var sequelize = require("sequelize");
+// var sequelize = require("sequelize");
 
-if(process.env.JAWSDB_URL) {
-  //Heroku deployment
-   var connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if(process.env.JAWSDB_URL) {
+//   //Heroku deployment
+//    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 
-    var sequelize = new sequelize("users_db", "root", "Jjb117jjB",{
+//     var sequelize = new sequelize("users_db", "root", "Jjb117jjB",{
 
-        host: "localhost",
-        port: 3306,
-        dialect: "mysql",
+//         host: "localhost",
+//         port: 3306,
+//         dialect: "mysql",
         
-            pool:{
-                max: 5,
-                min: 0,
-                idle: 10000
-            }
-    });
-}
+//             pool:{
+//                 max: 5,
+//                 min: 0,
+//                 idle: 10000
+//             }
+//     });
+// }
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+var Sequelize = require('sequelize'), connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize('users_db', 'root', 'Jjb117jjB', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306'
+  })
+};
+
+module.exports = connection;
